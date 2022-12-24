0b28156e84f6a012285459dbafcc1eee5bf95db4
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-12-25 02:53:59 +0800

    feat: Add function to list all plugins

--- naevpm/__init__.py
+++ naevpm/__init__.py
@@ -1,3 +1,4 @@
 from naevpm import database
 from naevpm import directories
 from naevpm import plugin_registry
+from naevpm import plugin
---
+++ naevpm/plugin.py
@@ -0,0 +1,37 @@
+import os
+from naevpm import plugin_registry
+import xml.etree.cElementTree as ET
+
+PLUGIN_DIR = "plugins"
+
+
+def list_all_plugins():
+    registries = plugin_registry.get_registries_from_database()
+
+    plugins = []
+
+    for registry in registries:
+        plugins += all_plugins_in_registry(registry[1])
+
+    for plugin in plugins:
+        print(plugin)
+
+
+def all_xml_files_in_directory(dir: str):
+    return [filename for filename in os.listdir(dir) if filename.endswith('.xml')]
+
+
+def parse_plugin_xml_file(file):
+    tree = ET.parse(file)
+    plugin = tree.getroot()
+    return {
+        "name": plugin.attrib.get("name"),
+        "author": plugin.findtext("author"),
+        "git": plugin.findtext("git")
+    }
+
+
+def all_plugins_in_registry(registry_dir):
+    return [parse_plugin_xml_file(os.path.join(registry_dir, PLUGIN_DIR, xml_file))
+            for xml_file in all_xml_files_in_directory(
+                os.path.join(registry_dir, PLUGIN_DIR))]
