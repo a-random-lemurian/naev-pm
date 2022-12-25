739b5b72559c317a5e9ca2b02122f9577ad7a609
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-12-25 08:35:32 +0800

    feat: Add ability to update registries

--- naevpm/__init__.py
+++ naevpm/__init__.py
@@ -2,3 +2,4 @@ from naevpm import database
 from naevpm import directories
 from naevpm import plugin_registry
 from naevpm import plugin
+from naevpm import update
---
+++ naevpm/update.py
@@ -0,0 +1,59 @@
+import pygit2 as git
+from naevpm import plugin_registry
+
+
+# Taken from <https://github.com/MichaelBoselowitz/pygit2-examples/blob/master/examples.py>
+def git_repository_pull(repo, remote_name='origin', branch='main'):
+    for remote in repo.remotes:
+        if remote.name == remote_name:
+            remote.fetch()
+            remote_master_id = repo.lookup_reference('refs/remotes/origin/%s' % (branch)).target
+            merge_result, _ = repo.merge_analysis(remote_master_id)
+            # Up to date, do nothing
+            if merge_result & git.GIT_MERGE_ANALYSIS_UP_TO_DATE:
+                return
+            # We can just fastforward
+            elif merge_result & git.GIT_MERGE_ANALYSIS_FASTFORWARD:
+                repo.checkout_tree(repo.get(remote_master_id))
+                try:
+                    master_ref = repo.lookup_reference('refs/heads/%s' % (branch))
+                    master_ref.set_target(remote_master_id)
+                except KeyError:
+                    repo.create_branch(branch, repo.get(remote_master_id))
+                repo.head.set_target(remote_master_id)
+            elif merge_result & git.GIT_MERGE_ANALYSIS_NORMAL:
+                repo.merge(remote_master_id)
+
+                if repo.index.conflicts is not None:
+                    for conflict in repo.index.conflicts:
+                        print('Fatal: conflicts found in:'+conflict[0].path)
+                    raise AssertionError('Please resolve the conflict')
+
+                user = repo.default_signature
+                tree = repo.index.write_tree()
+                commit = repo.create_commit('HEAD',
+                                            user,
+                                            user,
+                                            'Merge!',
+                                            tree,
+                                            [repo.head.target, remote_master_id])
+                # We need to do this or git CLI will think we are still merging.
+                repo.state_cleanup()
+            else:
+                raise AssertionError('Unknown merge analysis result')
+
+
+def update_one_registry(registry_dir: str):
+    repo = git.Repository(registry_dir)
+    print(f"Updating registry ${registry_dir}")
+    git_repository_pull(repo)
+
+
+def update_registries():
+    """
+    Update the registries to receive updated information
+    from the GitHub plugin registries.
+    """
+    for registry in plugin_registry.get_registries_from_database():
+        update_one_registry(registry[1])
+
