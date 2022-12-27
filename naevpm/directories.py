b74b532a2650a7c4d0f8d1ce38f70326c16df929
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-12-27 22:16:38 +0800

    feat: Add Naev directory to NaevPMDirectories

--- naevpm/directories.py
+++ naevpm/directories.py
@@ -5,6 +5,8 @@ class NaevPMDirectories:
     ROOT = appdirs.user_data_dir("naev-package-manager")
     DATABASE = os.path.join(ROOT, "naevpm.db")
     REGISTRIES = os.path.join(ROOT, "registries")
+    NAEV_ROOT = appdirs.user_data_dir("naev")
+    NAEV_PLUGIN_DIR = os.path.join(NAEV_ROOT, "plugins")
 
 def make_registry_directory(name: str):
     return os.path.join(NaevPMDirectories.REGISTRIES, name)
